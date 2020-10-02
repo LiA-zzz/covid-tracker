5b5eacc3535b6991f2b149886beb5b8d14fe77e0
Andy Li <52589141+LiA-zzz@users.noreply.github.com>
2020-10-01 17:35:36 -0700

    fix: day is zero when at start of month and before update occurs

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -19,11 +19,37 @@ export async function fetchUSAData() {
 }
 
 function formatDateYMD(date) {
-  const d =
+  const daysPerMonth = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 31,
+    12: 31,
+  };
+  let d =
     date.getUTCHours() <= 6 ? date.getUTCDate() - 2 : date.getUTCDate() - 1;
-  const m = date.getUTCMonth() + 1;
+  let m = date.getUTCMonth() + 1;
   const y = date.getUTCFullYear();
+  if (d === 0) {
+    m = m - 1 > 0 ? m - 1 : 12;
+    d =
+      m === 2
+        ? isLeapYear(y)
+          ? daysPerMonth[m] + 1
+          : daysPerMonth[m]
+        : daysPerMonth[m];
+  }
   return (m < 10 ? "0" + m : m) + "-" + d + "-" + y;
 }
 
+function isLeapYear(year) {
+  return year % 4 === 0 && year % 100 !== 0 && year % 400 === 0;
+}
 export default { fetchAllData, fetchUSAData };
